866b0257f0173d535d34168dda5262f00ac1d11f
Olesya <lesusha1@yandex.ru>
2022-09-25 23:56:52 +0300

    add Task_40

--- Task_40/Program.cs
+++ Task_40/Program.cs
@@ -2,16 +2,21 @@
 // может ли существовать треугольник с сторонами такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 
-Console.WriteLine("Введите 3 числа через пробел: ");
+Console.Write("Введите 3 числа через пробел: ");
 string input = Console.ReadLine()!;
 string[] numbers = input.Split(' ');
 int a = int.Parse(numbers[0]);
 int b = int.Parse(numbers[1]);
 int c = int.Parse(numbers[2]);
 
-
-if (a < b + a && b < a + c && c < a + b)
+if (DoesTriangleExist(a, b, c))
 {
     Console.WriteLine("yes");
 }
 else Console.WriteLine("no");
+
+
+bool DoesTriangleExist(int a, int b, int c)
+{
+    return a < b + a && b < a + c && c < a + b;
+}
